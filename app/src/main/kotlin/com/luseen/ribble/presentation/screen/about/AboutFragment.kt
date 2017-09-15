aacc9db3c6b8418844b1c210338c5afb83502889
Arman Chatikyan <armcha01@gmail.com>
2017-09-15 22:15:07 +0400

    Added About fragment content

--- app/src/main/kotlin/com/luseen/ribble/presentation/screen/about/AboutFragment.kt
+++ app/src/main/kotlin/com/luseen/ribble/presentation/screen/about/AboutFragment.kt
@@ -2,13 +2,18 @@ package com.luseen.ribble.presentation.screen.about
 
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.View
 import android.widget.TextView
+import com.luseen.ribble.BuildConfig
 import com.luseen.ribble.R
 import com.luseen.ribble.presentation.base_mvp.base.BaseFragment
 import com.luseen.ribble.presentation.widget.navigation_view.NavigationId
+import com.luseen.ribble.utils.S
+import com.luseen.ribble.utils.extensions.actionView
 import com.luseen.ribble.utils.extensions.iconTint
 import com.luseen.ribble.utils.extensions.leftIcon
+import com.luseen.ribble.utils.extensions.sendEmail
 import kotlinx.android.synthetic.main.fragment_about.*
 import javax.inject.Inject
 
@@ -45,36 +50,49 @@ class AboutFragment : BaseFragment<AboutContract.View, AboutContract.Presenter>(
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (2 until 9 step 2)
+        (3 until 10 step 2)
                 .map { rootView.getChildAt(it) as TextView }
                 .onEach { it.setOnClickListener(this) }
                 .forEachIndexed { current, textView ->
-                    val aboutItem = items[current]
+                    val (stringRes, icon, tintColor) = items[current]
                     with(textView) {
-                        text = getString(aboutItem.stringResId)
-                        leftIcon(aboutItem.icon)
-                        iconTint(aboutItem.tintColor)
+                        text = getString(stringRes)
+                        leftIcon(icon)
+                        iconTint(tintColor)
                         id = current
                     }
                 }
+        appInfo.text = """${getString(S.app_name)} ${BuildConfig.VERSION_NAME}
+            |Copyright © 2014-2017
+            |Arman Chatikyan""".trimMargin()
     }
 
     override fun onClick(view: View) {
+
         when (view.id) {
             EMAIL -> {
-
+                context.sendEmail(getString(S.app_name), "", getString(R.string.sen_us_emial))
             }
             TWITTER -> {
-
+                context.actionView {
+                    "https://twitter.com/ArmanChatikyan"
+                }
             }
             FACEBOOK -> {
-
+                context.actionView {
+                    "https://web.facebook.com/chatikyana"
+                }
             }
             GITHUB -> {
-
+                context.actionView {
+                    "https://github.com/armcha/Ribble"
+                }
             }
         }
     }
 }
+
+operator fun Fragment.get(id: Int): String = getString(id)
+
 
 data class AboutItem(val stringResId: Int, val icon: Int, val tintColor: Int = 0)
---
+++ app/src/main/kotlin/com/luseen/ribble/utils/Typealias.kt
@@ -0,0 +1,10 @@
+package com.luseen.ribble.utils
+
+import com.luseen.arch.R
+
+/**
+ * Created by Chatikyan on 15.09.2017.
+ */
+
+typealias S = R.string
+typealias C = R.color
